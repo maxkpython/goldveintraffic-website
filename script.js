ab76ad8b9406a8363c73dc153accce506679048d
maxkpython <mkravchuk@tuta.com>
2026-08-07 21:41:03 -0300

    Add files via upload

--- script.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById("demoButton");
-const message = document.getElementById("message");
-
-button.addEventListener("click", () => {
-    message.textContent = "JavaScript is working! 🚀";
-});
